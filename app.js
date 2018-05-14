2f5639a0c29aa5fa4ad198399dac139d30f6a885
Atsushi Nakatsugawa <atsushi@moongift.jp>
2018-05-14 10:12:32 +0900

    Add env

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 setInterval(() => {
-  console.log("Hello, Isaax. I'm Raspberry Pi!")
+  console.log("Hello, Isaax. I'm Raspberry Pi!");
+  console.log(`My env is ${process.env.MY_ENV}`);
 }, 5000);
