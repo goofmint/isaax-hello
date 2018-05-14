eed6742c0cecdd1129545113f8eb7bd17e21a9f2
Atsushi Nakatsugawa <atsushi@moongift.jp>
2018-05-14 10:18:59 +0900

    Update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 setInterval(() => {
   console.log("Hello, Isaax. I'm Raspberry Pi!");
   console.log(`My env is ${process.env.MY_ENV}`);
-  console.log('Updated.')
+  console.log('Updated')
 }, 5000);
