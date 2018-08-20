8141ac5b1385459ac008cc6a2a280b4ada526729
Atsushi <atsushi@moongift.jp>
2018-08-20 22:38:41 +0900

    Add file

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from pyfirmata import Arduino, util
 from time import sleep
 board = Arduino('/dev/ttyS0')
-print "Start blinking D4"
+print "Start blinking D4!"
 while True:
     board.digital[4].write(1)
     sleep(0.5)
